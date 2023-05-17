38031fcd57ca14dfb123a2611632d160db689568
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-05-17 14:33:29 -0300

    wip: show screen size for debugging

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -7,59 +7,72 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 25),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 120),
-            child: NavBar(),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                const SizedBox(width: 120),
-                Flexible(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: DsText.heading1(
-                          'Subscribe today and get notified first when we launch Orange app!',
-                          color: DsColors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      Flexible(
-                        child: DsText.bodyEmphasis(
-                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-                          color: DsColors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-                      Row(
+    return Stack(
+      children: [
+        Scaffold(
+          body: Column(
+            children: [
+              const SizedBox(height: 25),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 120),
+                child: NavBar(),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(width: 120),
+                    Flexible(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Expanded(child: TextField()),
-                          const SizedBox(width: 16),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Subscribe now'),
+                          Flexible(
+                            child: DsText.heading1(
+                              'Subscribe today and get notified first when we launch Orange app!',
+                              color: DsColors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 25),
+                          Flexible(
+                            child: DsText.bodyEmphasis(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+                              color: DsColors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                          Row(
+                            children: [
+                              const Expanded(child: TextField()),
+                              const SizedBox(width: 16),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Subscribe now'),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 88),
+                    Flexible(
+                      child: Image.asset('assets/mobile.png'),
+                    ),
+                    const SizedBox(width: 50),
+                  ],
                 ),
-                const SizedBox(width: 88),
-                Flexible(
-                  child: Image.asset('assets/mobile.png'),
-                ),
-                const SizedBox(width: 50),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Material(
+            color: Colors.transparent,
+            child: DsText.bodyEmphasis(
+                '${constraints.maxWidth}x${constraints.maxHeight}',
+                color: DsColors.white),
+          );
+        }),
+      ],
     );
   }
 }
